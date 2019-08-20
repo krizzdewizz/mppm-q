e6ca1c4a983e999393fa28f470ad08168b805dc7
Christian Oetterli <christian@o3tt3rli.com>
2019-08-21 00:22:37 +0200

    yt

---
+++ YoutubeMp3Downloader.js
@@ -0,0 +1,151 @@
+'use strict';
+const os = require('os');
+const util = require('util');
+const EventEmitter = require('events').EventEmitter;
+const ytdl = require('ytdl-core');
+const async = require('async');
+const sanitize = require('sanitize-filename');
+const ffmpeg = require('ffmpeg.js/ffmpeg-mp4.js');
+
+function concatenate(resultConstructor, ...arrays) {
+    let totalLength = 0;
+    for (const arr of arrays) {
+        totalLength += arr.length;
+    }
+    const result = new resultConstructor(totalLength);
+    let offset = 0;
+    for (const arr of arrays) {
+        result.set(arr, offset);
+        offset += arr.length;
+    }
+    return result;
+}
+
+function YoutubeMp3Downloader(options) {
+
+    var self = this;
+
+    self.youtubeBaseUrl = 'http://www.youtube.com/watch?v=';
+    self.youtubeVideoQuality = (options && options.youtubeVideoQuality ? options.youtubeVideoQuality : 'highest');
+    self.queueParallelism = (options && options.queueParallelism ? options.queueParallelism : 1);
+    self.progressTimeout = (options && options.progressTimeout ? options.progressTimeout : 1000);
+    self.fileNameReplacements = [[/"/g, ''], [/\|/g, ''], [/'/g, ''], [/\//g, ''], [/\?/g, ''], [/:/g, ''], [/;/g, '']];
+    self.requestOptions = (options && options.requestOptions ? options.requestOptions : { maxRedirects: 5 });
+    self.outputOptions = (options && options.outputOptions ? options.outputOptions : []);
+
+    if (options && options.ffmpegPath) {
+        ffmpeg.setFfmpegPath(options.ffmpegPath);
+    }
+
+    self.downloadQueue = async.queue(function (task, callback) {
+
+        self.emit('queueSize', self.downloadQueue.running() + self.downloadQueue.length());
+
+        self.performDownload(task, function (err, result) {
+            callback(err, result);
+        });
+
+    }, self.queueParallelism);
+
+}
+
+util.inherits(YoutubeMp3Downloader, EventEmitter);
+
+YoutubeMp3Downloader.prototype.cleanFileName = function (fileName) {
+    var self = this;
+
+    self.fileNameReplacements.forEach(function (replacement) {
+        fileName = fileName.replace(replacement[0], replacement[1]);
+    });
+
+    return fileName;
+};
+
+YoutubeMp3Downloader.prototype.download = function (videoId, fileName) {
+
+    var self = this;
+    var task = {
+        videoId: videoId,
+        fileName: fileName
+    };
+
+    self.downloadQueue.push(task, function (err, data) {
+
+        self.emit('queueSize', self.downloadQueue.running() + self.downloadQueue.length());
+
+        if (err) {
+            self.emit('error', err, data);
+        } else {
+            self.emit('finished', err, data);
+        }
+    });
+
+};
+
+YoutubeMp3Downloader.prototype.performDownload = function (task, callback) {
+
+    var self = this;
+    var videoUrl = self.youtubeBaseUrl + task.videoId;
+    var resultObj = {
+        videoId: task.videoId
+    };
+
+    ytdl.getInfo(videoUrl, function (err, info) {
+
+        if (err) {
+            callback(err.message, resultObj);
+        } else {
+            // Map new structure to old one
+            info = info.player_response.videoDetails;
+
+            var videoTitle = self.cleanFileName(info.title);
+            var artist = 'Unknown';
+            var title = 'Unknown';
+            var thumbnail = info.thumbnail.thumbnails[0].url || null;
+
+            if (videoTitle.indexOf('-') > -1) {
+                var temp = videoTitle.split('-');
+                if (temp.length >= 2) {
+                    artist = temp[0].trim();
+                    title = temp[1].trim();
+                }
+            } else {
+                title = videoTitle;
+            }
+
+            const fileName = (task.fileName ? task.fileName : (sanitize(videoTitle) || info.videoId) + '.mp3');
+
+            ytdl.getInfo(videoUrl, { quality: self.youtubeVideoQuality }, function (err, info) {
+
+                if (err) callback(err, null);
+
+                const stream = ytdl.downloadFromInfo(info, {
+                    quality: self.youtubeVideoQuality,
+                    requestOptions: self.requestOptions
+                });
+
+                let data = new Uint8Array(0);
+                const inFile = 'qbert.webm';
+
+                stream.on('data', d => {
+                    data = concatenate(Uint8Array, data, new Uint8Array(d));
+                });
+
+                stream.on('end', () => {
+                    const result = ffmpeg({
+                        MEMFS: [{ name: inFile, data }],
+                        arguments: ['-i', inFile, '-vn', 'q.mp3'],
+                        stdin: () => undefined,
+                    });
+
+                    const out = result.MEMFS[0];
+                    callback(undefined, { buffer: Buffer.from(out.data), fileName});
+                    //fs.writeFileSync('d:/downloads/vv/xxx.mp3', Buffer(out.data));
+                });
+            });
+        }
+    });
+
+};
+
+module.exports = YoutubeMp3Downloader;
--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const YoutubeMp3Downloader = require('./YoutubeMp3Downloader');
+
+function yt(req, res) {
+
+    const YD = new YoutubeMp3Downloader({
+        'youtubeVideoQuality': 'highest',
+        'queueParallelism': 1,
+        'progressTimeout': 2000
+    });
+
+    const videoId = req.query.vid;
+
+    if (!videoId) {
+        return res.end('missing query parameter vid');
+    }
+
+    YD.download(videoId);
+
+    YD.on('finished', (err, data) => {
+        const { fileName, buffer } = data;
+        console.log('fileName', fileName);
+        res.setHeader('Content-disposition', `attachment; filename=${fileName}`);
+        res.setHeader('Content-type', 'audio/mpeg');
+        res.end(buffer);
+    });
+
+    // YD.on('error', function(error) {
+    //     console.log(error);
+    // });
+
+    // YD.on('progress', function(progress) {
+    //     console.log(JSON.stringify(progress));
+    // });
+}
+
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .get('/yt', yt)
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
