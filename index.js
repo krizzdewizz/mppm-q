3abf31d761bac8c6af49170412a0128712a89376
christian.oetterli <christian@o3tt3rli.com>
2022-04-12 23:10:19 +0200

    use background job

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const ytdl = require('ytdl-core');
 const ffmpeg = require('ffmpeg.js/ffmpeg-mp4.js');
 
 const PORT = process.env.PORT || 5000;
-process.env.YTDL_NO_UPDATE='true'
+process.env.YTDL_NO_UPDATE = 'true';
+
+let job;
+let jobVideoId;
+let jobResult;
 
 function concatenate(resultConstructor, ...arrays) {
   let totalLength = 0;
@@ -19,7 +23,7 @@ function concatenate(resultConstructor, ...arrays) {
   return result;
 }
 
-function ytGet(req, res) {
+function ytDownload(req, res) {
 
   const videoId = req.query.vid;
 
@@ -27,34 +31,71 @@ function ytGet(req, res) {
     return res.end(`missing query parameter 'vid'`);
   }
 
-  console.log(`downloading ${videoId}...`);
+  if (job) {
+    return res.end(`download job already running`);
+  }
 
-  // https://www.youtube.com/watch?v=vx2u5uUu3DE
-  const stream = ytdl(`https://www.youtube.com/watch?v=${videoId}`);
+  jobVideoId = videoId;
 
-  let data = new Uint8Array(0);
+  job = new Promise((resolve, reject) => {
 
-  stream.on('data', d => {
-    data = concatenate(Uint8Array, data, new Uint8Array(d));
-  });
+    console.log(`downloading ${videoId}...`);
 
-  stream.on('end', () => {
-    console.log(`done downloading ${videoId}. Extracting audio...`);
+    // https://www.youtube.com/watch?v=vx2u5uUu3DE
+    const stream = ytdl(`https://www.youtube.com/watch?v=${videoId}`);
 
-    const inFile = 'qbert.webm';
-    const result = ffmpeg({
-      MEMFS: [{name: inFile, data}],
-      arguments: ['-i', inFile, '-vn', 'q.mp3'],
-      stdin: () => undefined,
+    let data = new Uint8Array(0);
+
+    stream.on('data', d => {
+      data = concatenate(Uint8Array, data, new Uint8Array(d));
     });
 
-    const out = result.MEMFS[0];
-    // fs.writeFileSync('tmp.mp3', Buffer.from(out.data));
+    stream.on('error', reject);
 
-    res.setHeader('Content-disposition', `attachment; filename=${videoId}.mp3`);
-    res.setHeader('Content-type', 'audio/mpeg');
-    res.end(Buffer.from(out.data));
+    stream.on('end', () => {
+      console.log(`done downloading ${videoId}. Extracting audio...`);
+
+      try {
+        const inFile = 'qbert.webm';
+        const result = ffmpeg({
+          MEMFS: [{name: inFile, data}],
+          arguments: ['-i', inFile, '-vn', 'q.mp3'],
+          stdin: () => undefined,
+        });
+
+        const out = result.MEMFS[0];
+        // fs.writeFileSync('tmp.mp3', Buffer.from(out.data));
+
+        resolve(Buffer.from(out.data));
+      } catch (err) {
+        reject(err);
+      }
+    });
   });
+
+  job.then(data => jobResult = data)
+    .catch(err => console.error(`error while downloading ${videoId}`, err));
+
+  res.end(`download job for ${videoId} is now running. Call /ytget later to retrieve the data`);
+}
+
+function ytGet(req, res) {
+  if (!job) {
+    res.statusCode = 404;
+    return res.end(`no download job running`);
+  }
+
+  if (!jobResult) {
+    res.statusCode = 404;
+    return res.end(`job has not completed yet`);
+  }
+
+  res.setHeader('Content-disposition', `attachment; filename=${jobVideoId}.mp3`);
+  res.setHeader('Content-type', 'audio/mpeg');
+  res.end(jobResult);
+
+  jobResult = undefined;
+  jobVideoId = undefined;
 }
 
 express()
@@ -63,6 +104,7 @@ express()
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   })
+  .get('/ytdownload', ytDownload)
   .get('/ytget', ytGet)
   .listen(PORT, () => console.log(`listening on ${PORT}`));
 
