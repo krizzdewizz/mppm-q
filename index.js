51f4a58b7aae0fd56a840f2052b14b86969f3d6d
christian.oetterli <christian@o3tt3rli.com>
2022-04-12 22:37:50 +0200

    simplify, update to latest ytdl-core

--- index.js
+++ index.js
@@ -1,126 +1,68 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg.js/ffmpeg-mp4.js');
 
-const YoutubeMp3Downloader = require('./dl');
+const PORT = process.env.PORT || 5000;
+process.env.YTDL_NO_UPDATE='true'
 
-const queue = {};
-const IN_PROGRESS = '<<>>';
-const YT_ERROR = '!!!!';
-
-function yt(req, res) {
-
-    const videoId = req.query.vid;
-
-    if (!videoId) {
-        return res.end('missing query parameter "vid"');
-    }
-
-    if (queue[videoId]) {
-        // already queued
-        res.sendStatus(403);
-        return;
-    }
-
-    const YD = new YoutubeMp3Downloader();
-
-    queue[videoId] = IN_PROGRESS;
-
-    YD.download(videoId);
-
-    YD.on('finished', (err, data) => {
-        const { fileName, buffer } = data;
-        const filePath = path.join(__dirname, 'tmp', `${videoId}_${Date.now()}`);
-        fs.writeFileSync(filePath, buffer);
-        console.log('saving ', filePath);
-        queue[videoId] = { filePath, fileName };
-
-        setTimeout(() => {
-            console.log('expired ', filePath);
-            delete queue[videoId];
-            try {
-                fs.unlinkSync(filePath);
-            } catch {
-                // ignore, already been downloaded
-            }
-        }, 300000);
-    });
-
-    res.end(JSON.stringify({ videoId }));
-
-    YD.on('error', error => {
-        console.log('yt error', error);
-        queue[videoId] = YT_ERROR;
-    });
-}
-
-function ytReady(req, res) {
-
-    const videoId = req.query.vid;
-
-    if (!videoId) {
-        return res.end('missing query parameter "vid"');
-    }
-
-    const task = queue[videoId];
-
-    if (task === YT_ERROR) {
-        res.end(JSON.stringify({ ytError: true }))
-        return;
-    }
-
-    if (!task || task === IN_PROGRESS) {
-        res.sendStatus(404);
-        return;
-    }
-
-    res.end(JSON.stringify({ videoId }));
+function concatenate(resultConstructor, ...arrays) {
+  let totalLength = 0;
+  for (const arr of arrays) {
+    totalLength += arr.length;
+  }
+  const result = new resultConstructor(totalLength);
+  let offset = 0;
+  for (const arr of arrays) {
+    result.set(arr, offset);
+    offset += arr.length;
+  }
+  return result;
 }
 
 function ytGet(req, res) {
 
-    const videoId = req.query.vid;
+  const videoId = req.query.vid;
 
-    if (!videoId) {
-        return res.end('missing query parameter "vid"');
-    }
+  if (!videoId) {
+    return res.end(`missing query parameter 'vid'`);
+  }
 
-    const task = queue[videoId];
+  console.log(`downloading ${videoId}...`);
 
-    if (typeof task === 'string') {
-        res.sendStatus(404);
-        return;
-    }
+  // https://www.youtube.com/watch?v=vx2u5uUu3DE
+  const stream = ytdl(`https://www.youtube.com/watch?v=${videoId}`);
 
-    res.setHeader('Content-disposition', `attachment; filename=${task.fileName}`);
-    res.setHeader('Content-type', 'audio/mpeg');
-    const stream = fs.createReadStream(task.filePath);
+  let data = new Uint8Array(0);
 
-    stream.on('end', () => {
-        console.log('deleting ', task.filePath);
-        delete queue[videoId];
-        try {
-            fs.unlinkSync(task.filePath);
-        } catch {
-            // ignore
-        }
+  stream.on('data', d => {
+    data = concatenate(Uint8Array, data, new Uint8Array(d));
+  });
 
-        global.gc();
+  stream.on('end', () => {
+    console.log(`done downloading ${videoId}. Extracting audio...`);
+
+    const inFile = 'qbert.webm';
+    const result = ffmpeg({
+      MEMFS: [{name: inFile, data}],
+      arguments: ['-i', inFile, '-vn', 'q.mp3'],
+      stdin: () => undefined,
     });
 
-    stream.pipe(res);
+    const out = result.MEMFS[0];
+    // fs.writeFileSync('tmp.mp3', Buffer.from(out.data));
+
+    res.setHeader('Content-disposition', `attachment; filename=${videoId}.mp3`);
+    res.setHeader('Content-type', 'audio/mpeg');
+    res.end(Buffer.from(out.data));
+  });
 }
 
 express()
-    .use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-    .get('/yt', yt)
-    .get('/ytready', ytReady)
-    .get('/ytget', ytGet)
-    .listen(PORT, () => console.log(`Listening on ${PORT}`))
-
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  })
+  .get('/ytget', ytGet)
+  .listen(PORT, () => console.log(`listening on ${PORT}`));
 
